9fa1480d890da58a57aa4e17d51bab4f4232ed9e
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2023-07-23 13:20:59 +0530

    Part of Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="container mx-auto">
+      <section className="py-10 flex flex-col justify-center items-center ">
+        {/* Column 1 - Photo */}
+        <div className="md:w-1/2 relative">
+          <Image
+            src="/profile.jpeg"
+            width={500}
+            height={500}
+            alt="Picture of the author"
+            className="w-full h-full rounded-full"
+          />
+          <p className="m-3 text-center font-thin italic ">
+            Professional Image will arrive in 3-4 business days
+          </p>
+        </div>
+
+        {/* Column 2 - Text */}
+        <div className="flex flex-col items-center justify-center p-8">
+          <p className="text-5xl  text-center font-semibold mb-4">
+            Hello, Digital Adventurer!
+          </p>
+          <p className="w-2/3 text-center text-gray-600">
+            Myself in 10 seconds: An enthusiastic explorer in the realm of
+            technology, driven by boundless curiosity and a passion for
+            innovation. BTW, I love Spider-man
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,43 @@
+// src/components/Navbar.js
+
+import React, { useEffect, useState } from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="text-amber-900 p-5 bg-white bg-opacity-70 backdrop-blur-md rounded-b-md shadow-md fixed top-0 left-0 right-0">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <div className=" font-bold text-xl">Mohamed Aslam</div>
+          <div className="flex space-x-4">
+            <a
+              href="#home"
+              className=" hover:text-gray-200 transition duration-300"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className=" hover:text-gray-200 transition duration-300"
+            >
+              About
+            </a>
+            <a
+              href="#portfolio"
+              className=" hover:text-gray-200 transition duration-300"
+            >
+              Portfolio
+            </a>
+            <a
+              href="#contact"
+              className=" hover:text-gray-200 transition duration-300"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
