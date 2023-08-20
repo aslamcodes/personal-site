1375643a336802954ae442b35451266e14dbbd59
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2023-08-20 12:03:32 +0530

    feat(Functionalities): :sparkles: About will hidden on other pages from navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,22 +1,38 @@
+"use client";
+import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const path = usePathname();
+
   return (
     <nav className="text-amber-900 p-5 bg-white bg-opacity-70 backdrop-blur-md rounded-b-md shadow-md fixed z-50 top-0 left-0 right-0">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="font-bold text-xl mb-4 md:mb-0">Mohamed Aslam</div>
           <div className="md:flex  space-x-2 md:space-x-4">
-            <a href="#home" className="hover:text-gray-200 duration-300">
-              Home
-            </a>
-            <a href="#about" className="hover:text-gray-200 duration-300">
-              About
-            </a>
-            <a href="#portfolio" className="hover:text-gray-200 duration-300">
+            {path === "/" ? (
+              <>
+                <a href="#home" className="hover:text-gray-200 duration-300">
+                  Home
+                </a>
+                <a href="#about" className="hover:text-gray-200 duration-300">
+                  About
+                </a>
+              </>
+            ) : (
+              <Link href="/">Home</Link>
+            )}
+            <Link
+              href="/portfolio"
+              className="hover:text-gray-200 duration-300"
+            >
               Portfolio
-            </a>
-            <a className="hover:text-gray-200 duration-300">Blogs</a>
+            </Link>
+            <Link href="/blogs" className="hover:text-gray-200 duration-300">
+              Blogs
+            </Link>
           </div>
         </div>
       </div>
